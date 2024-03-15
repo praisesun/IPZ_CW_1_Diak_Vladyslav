545551af34713c6399703fd42f6d5eebccd66c29
praisesun <blackstar934589@gmail.com>
2024-03-15 16:09:58 +0200

    додаємо перегляд

--- app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
@@ -3,12 +3,15 @@ package ua.edu.lntu.cw_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw_1.ui.theme.IPZ_CW_1_Diak_VladyslavTheme
 
@@ -22,7 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Image(painter = painterResource(id = R.drawable.android_logo), contentDescription = "")
+                    }
                 }
             }
         }
